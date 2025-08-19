a16549085410e5373ff5f94903c83f19e8d1308f
brbr-prime <11265201+BrBr-Prime@users.noreply.github.com>
2025-08-18 21:03:58 -0700

    added bullets to shoot asteroids and the asteroids disappear

--- asteroid.py
+++ asteroid.py
@@ -15,3 +15,15 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+class Shot(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
+        self.velocity = 0
+        pygame.Vector2(0, 1)
+
+    def update(self, dt):
+        self.position += (self.velocity * dt)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
